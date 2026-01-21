a5c8890734ac1ac3734393462f78435156e0ab4b
GabrielDaiu7 <gabrieldaiu909@gmail.com>
2026-01-21 14:08:57 +0100

    Update

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,16 @@
+const today = 'today';
+
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    return [task, task2];
+  }
+
+  console.log(today);
+
+  return [task, task2];
+}
