499c306fe73716c519d836ad8eccaa65d58274c6
GabrielDaiu7 <gabrieldaiu909@gmail.com>
2026-01-21 14:27:13 +0100

    Update

---
+++ ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import GetNeighborhoodsList from './2-arrow';
+
+const NeighborhoodsList = new GetNeighborhoodsList();
+const res = NeighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
