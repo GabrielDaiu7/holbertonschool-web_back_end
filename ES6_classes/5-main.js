2fdcfd79773f283d43931edd56e027cc7dd3c440
GabrielDaiu7 <gabrieldaiu909@gmail.com>
2026-01-22 17:29:08 +0100

    update

---
+++ ES6_classes/5-main.js
@@ -0,0 +1,15 @@
+import Building from './5-building';
+
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
+
+try {
+  const a = new TestBuilding(200);
+  console.log(a);
+}
+
+catch (err) {
+  console.log(err);
+}
